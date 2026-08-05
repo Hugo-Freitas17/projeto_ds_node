b8768921154b00634c1db0ce83712274e4131896
Hugo-Freitas17 <hugo.silva16@aluno.cps.sp.gov.br>
2026-08-04 22:19:38 -0300

    Teste de confirmação de alteração

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express ();
+
+app.get('/', (req, res) => {
+	res.send('API funcionando');
+});
+
+app.get('/usuarios', (req, res) => {
+	res.json([
+		(id: 1, nome: 'João'),
+		(id: 2, nome: 'Maria')
+	]);
+});
+
+
