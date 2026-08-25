b0fa6f89469bc0e4c4329ba8e494beb8001f4664
Hugo-Freitas17 <hugo.silva16@aluno.cps.sp.gov.br>
2026-08-24 22:27:14 -0300

    Add files via upload

---
+++ bd/scripts.sql
@@ -0,0 +1,14 @@
+-- 
+CREATE DATABASE projeto_backend_angela;
+--
+USE projeto_backend_angela;
+--
+CREATE TABLE usuarios (
+    id_usuario INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR (50),
+    login VARCHAR (50),
+
+
+);
+--
+
