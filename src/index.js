9d7a9b203632bf3809e35bd653d8b0dc44ab9338
Hugo-Freitas17 <hugo.silva16@aluno.cps.sp.gov.br>
2026-08-25 21:52:58 -0300

    Update API response messages and format users data

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ const express = require('express');
 const app = express ();
 
 app.get('/', (req, res) => {
-	res.send('API funcionando');
+	res.send('API funcionando...');
 });
 
 app.get('/usuarios', (req, res) => {
 	res.json([
-		(id: 1, nome: 'João'),
-		(id: 2, nome: 'Maria')
+		{id: 1, nome: 'João'},
+		{id: 2, nome: 'Maria'}
 	]);
 });
 
-
+module.exports = app;
